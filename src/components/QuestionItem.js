73c1375225cd19f4c77a41adb025a548b4fcc84a
Riko-04 <echoge11@gmail.com>
2024-04-30 21:46:39 +0300

    Completes the lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
 
-  const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
-  ));
+  function handleDelete() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(() => {
+        // Call onDeleteQuestion to update state in parent component
+        onDeleteQuestion(id);
+      })
+      .catch((error) => {
+        console.error("Error deleting question:", error);
+      });
+  }
 
   return (
     <li>
@@ -15,9 +23,15 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex}>
+          {answers.map((answer, index) => (
+            <option key={index} value={index}>
+              {answer}
+            </option>
+          ))}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
